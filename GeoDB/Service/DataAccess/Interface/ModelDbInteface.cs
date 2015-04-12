1d3b260907f55f9f631656a57717daa44f8e86d1
AleksanderGalkin <agalkinov@mail.ru>
2015-04-12 23:20:38 +0700

    БД

---
+++ GeoDB/Service/DataAccess/Interface/ModelDbInteface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoDB.Service.DataAccess.Interface
+{
+    interface IModelDb<T>
+        where T:class
+    {
+
+    }
+}
