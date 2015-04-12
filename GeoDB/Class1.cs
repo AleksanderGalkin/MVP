d8e8f35c48047f945f548a8bfe056c7f0b683a4a
AleksanderGalkin <agalkinov@mail.ru>
2015-04-12 18:58:54 +0700

    БД

--- GeoDB/Class1.cs
+++ GeoDB/Class1.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace GeoDB
 {
-    public class Class1
-    {
-    }
+
+        static class Program
+        {
+            [STAThread]
+            static void Main()
+            {
+               // FormView view = new FormView();
+               // Presenter presenter = new Presenter(view);
+                   Application.Run();
+               // presenter.Show();
+            }
+        }
+    
 }
