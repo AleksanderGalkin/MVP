5a8b950459d121c36145232f4b96efe608f3c54e
AleksanderGalkin <agalkinov@mail.ru>
2015-04-28 11:20:45 +0700

    Попытка реализовать IBrowseAnyModel

---
+++ GeoDB/View/IBrowseAnyModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoDB.Model;
+using GeoDB.Model.Interface;
+
+namespace GeoDB.View
+{
+    public interface IBrowseAnyModel<T>
+        where T: class,IViewModel, new()
+    {
+        List<DGVHeader> Header { set;  }
+        Dictionary<int, T> Data { set; get; }
+        
+        int minShowedRow { set; get; }
+        int maxShowedRow { set; get; }
+        int rowCount { set; get; }
+
+        //void Show();
+        //void Close();
+
+
+        //event EventHandler<EventArgs> openForm;
+        event EventHandler<EventArgs> clickCollarList;
+        event EventHandler<EventArgs> showPrevScreen;
+        event EventHandler<EventArgs> showNextScreen;
+        event EventHandler<NumRowEventArgs> showAnyScreen;
+        event EventHandler<EventArgs> clickFilters;
+        event EventHandler<EventArgs> clickHeader;
+        //event EventHandler<EventArgs> clickCloseForm;
+        event EventHandler<NumRowEventArgs> setCurrentRow;
+
+
+    }
+}
---
+++ GeoDB/View/IViewAssays2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoDB.Model;
+
+namespace GeoDB.View
+{
+    public interface IViewAssays2
+    {
+        
+        List<DGVHeader> AssaysHeader { set; }
+        Dictionary<int, Assays2VmFull> AssaysList { set; get; }
+        int minShowedRow { set; get; }
+        int maxShowedRow { set; get; }
+        int rowCount { set; get; }
+
+        void Show();
+        void Close();
+
+
+        event EventHandler<EventArgs> openForm;
+        event EventHandler<EventArgs> clickCollarList;
+        event EventHandler<EventArgs> showPrevScreen;
+        event EventHandler<EventArgs> showNextScreen;
+        event EventHandler<NumRowEventArgs> showAnyScreen;
+        event EventHandler<EventArgs> clickFilters;
+        event EventHandler<EventArgs> clickHeader;
+        event EventHandler<EventArgs> clickCloseForm;
+
+
+    }
+}
